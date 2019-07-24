09d8583c558bc0303f5bc4bbcb3da0f5e35d7a44
ehmatthes <eric@example.com>
2019-07-24 15:14:36 +1200

    绘制一些列点

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter(2, 4, s=200)  # s是点的尺寸
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=100)  # s是点的尺寸
 
 # 设置图标标题并给坐标加上标签
 plt.title("Square Numbers", fontsize=24)
