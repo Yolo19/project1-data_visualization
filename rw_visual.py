3d007deac302b0f9374749f50e8a09a9d932d888
ehmatthes <eric@example.com>
2019-07-25 22:07:36 +1200

    随机漫步着色图

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,10 @@ while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_number = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_number, cmap=plt.cm.Blues,
+                edgecolor='none', s=15)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
