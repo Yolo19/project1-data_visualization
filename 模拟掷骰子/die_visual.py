f8dcaec52dcd38c74eec932410b03471779d24e5
ehmatthes <eric@example.com>
2019-07-26 16:48:30 +1200

    掷两个相同的骰子

--- 模拟掷骰子/die_visual.py
+++ 模拟掷骰子/die_visual.py
@@ -1,18 +1,20 @@
 from die import Die
 import pygal
 
-# 创建一个D6
-die = Die()
+#创建两个骰子
+die_1 = Die()
+die_2 = Die()
 
 # 掷几次骰子，并将结果存储在一个列表中
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # 分析结果
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -20,9 +22,9 @@ for value in range(1, die.num_sides+1):
 hist = pygal.Bar()
 
 hist.title = "Result of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = 'Result'
 hist.y_title = "Frequency of Result"
 
 hist.add('D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.render_to_file('dice_visual.svg')
