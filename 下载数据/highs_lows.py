aa17911450c86e77921696b82e114d3f02622a95
ehmatthes <eric@example.com>
2019-07-28 13:43:10 +1200

    给两个数据集之间的区域着色

--- 下载数据/highs_lows.py
+++ 下载数据/highs_lows.py
@@ -23,6 +23,7 @@ with open(filename) as f:
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
 plt.plot(dates, lows, c='blue')
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # 设置图形格式
 plt.title("Daily high and low temperatures - 2014", fontsize=24)
