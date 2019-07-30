c3ac7667bdbb1cb6157e0f53c4a89e94769a970b
ehmatthes <eric@example.com>
2019-07-30 23:03:43 +1200

    添加自定义的工具提示标签

--- 使用API/python_repos.py
+++ 使用API/python_repos.py
@@ -2,7 +2,6 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-
 # 执行API调用并存储响应
 URL = 'https://api.github.com/search/repositories?q=language:python&sort=star'
 r = requests.get(URL)  # 调用get() 并将URL传递给它，再将响应对象存储在变量r中
@@ -14,11 +13,16 @@ print("Total repositories:", response_dicts['total_count'])
 
 # 研究有关仓库的信息
 repo_dicts = response_dicts['items']
+print("Number of items:", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+    }
+    plot_dicts.append(plot_dict)
 
 #  可视化
 my_style = LS('#333366', base_style=LCS)
@@ -37,5 +41,6 @@ chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 chart.title = 'Most-Starred Python Project on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
+
