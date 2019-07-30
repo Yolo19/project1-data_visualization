b2a58ae16326dd278af55b64f657379ab2bcaecf
ehmatthes <eric@example.com>
2019-07-30 21:25:00 +1200

    使用pygal可视化仓库

--- 使用API/python_repos.py
+++ 使用API/python_repos.py
@@ -1,9 +1,11 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 # 执行API调用并存储响应
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r = requests.get(url)  # 调用get() 并将URL传递给它，再将响应对象存储在变量r中
+URL = 'https://api.github.com/search/repositories?q=language:python&sort=star'
+r = requests.get(URL)  # 调用get() 并将URL传递给它，再将响应对象存储在变量r中
 print("Status code:", r.status_code)
 
 # 将API响应存储在一个变量中
@@ -12,14 +14,17 @@ print("Total repositories:", response_dicts['total_count'])
 
 # 研究有关仓库的信息
 repo_dicts = response_dicts['items']
-print("Repositories returned:", len(repo_dicts))
 
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('Name:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+#  可视化
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Project on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
