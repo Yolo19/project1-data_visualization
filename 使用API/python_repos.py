a6e07624a2ad8fabd6dcad9266ce5f64965b4eba
ehmatthes <eric@example.com>
2019-07-30 21:06:03 +1200

    概述多个仓库

--- 使用API/python_repos.py
+++ 使用API/python_repos.py
@@ -10,25 +10,16 @@ print("Status code:", r.status_code)
 response_dicts = r.json()
 print("Total repositories:", response_dicts['total_count'])
 
-# 探索有关仓库的信息
+# 研究有关仓库的信息
 repo_dicts = response_dicts['items']
 print("Repositories returned:", len(repo_dicts))
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
-
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
-
-"""
-# 查看第一个仓库的信息
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
-"""
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
